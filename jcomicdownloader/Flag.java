83277a82ccc182e6ba9b06874f31a9a4716f5207
abc9070410@gmail.com <abc9070410@gmail.com@eb843f33-aada-1633-33ce-5fc2b7a079e5>
2011-10-25 23:32:50 +0000

    1.07

---
+++ jcomicdownloader/Flag.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcomicdownloader;
+
+/**
+ *
+ * @author user
+ */
+public class Flag {
+    public static boolean parseFlag = true;
+    public static boolean allowDownloadFlag= false;
+    public static boolean downloadingFlag= false;
+    public static boolean parseUrlFlag = false; // 分析結束
+}
