f082d77861d816a0842c75b6393900a3245ffef2
abc9070410@gmail.com <abc9070410@gmail.com@eb843f33-aada-1633-33ce-5fc2b7a079e5>
2011-10-25 23:39:54 +0000

    enum of sites

---
+++ jcomicdownloader/enums/Site.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcomicdownloader.enums;
+
+/**
+ *
+ * @author user
+ */
+/**
+ * 網站的代號，當作Enum用
+ * */
+public class Site {
+    public final static int UNKNOWN = 0;
+    public final static int CC = 1;
+    public final static int KUKU = 2;
+    public final static int EH = 3;
+    public final static int NINENINE_MANGA = 4;
+    public final static int NINENINE_COMIC = 5;
+    public final static int NINENINE_99770 = 6;
+    public final static int NINENINE_COCO = 7;
+    public final static int NINENINE_MH = 8;
+    public final static int NINENINE_1MH = 9;
+    public final static int NINENINE_3G = 10;
+
+}
