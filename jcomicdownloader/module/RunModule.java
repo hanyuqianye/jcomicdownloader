f0b0fbc55b88ce1e0631733aa7cfdab692120fa6
abc9070410@gmail.com <abc9070410@gmail.com@eb843f33-aada-1633-33ce-5fc2b7a079e5>
2011-10-25 23:49:41 +0000

    start to run the correct module

---
+++ jcomicdownloader/module/RunModule.java
@@ -0,0 +1,180 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcomicdownloader.module;
+
+import jcomicdownloader.tools.*;
+import jcomicdownloader.*;
+import jcomicdownloader.enums.*;
+
+import java.io.*;
+import java.util.*;
+import jcomicdownloader.Flag;
+import jcomicdownloader.SetUp;
+
+/**
+ *
+ * @author user
+ */
+public class RunModule {
+    private String indexName = "";
+    private String indexEncodeName = "";
+
+    public RunModule() {}
+
+
+    public synchronized void runMainProcess( ParseOnlineComicSite parse, 
+                                               String urlString ) {
+        parse.printLogo();
+
+        if ( urlString.matches( "(?s).*.htm" ) ||
+             urlString.matches( "(?s).*.html" ) ||
+             urlString.matches( "(?s).*.php" ) ||
+             urlString.matches( "(?s).*.asp" ) ||
+             urlString.matches( "(?s).*.jsp" ) ||
+             urlString.matches( "(?s).*/" ) );
+        else
+           urlString += "/";
+
+        Common.debugPrintln( "目前解析位址：" + urlString );
+ 
+        if ( parse.isSingleVolumePage( urlString ) ) { // ex. http://www.89890.com/comic/7953/
+            // the urlString is single volume page
+            Common.debugPrintln( "單集頁面（single volume）" );
+            Common.isMainPage = false;
+
+            parse.setTitle( parse.getTitleOnSingleVolumePage( urlString ) );
+            Common.debugPrintln( "漫畫名稱: " + parse.getTitle() );
+            //Common.nowTitle = parse.getTitle();
+
+            System.out.println( "++" + parse.getRunMode() + "++"  );
+            //if ( Flag.allowDownloadFlag && !Flag.downloadingFlag )
+            if ( parse.getRunMode() == RunModeEnum.DOWNLOAD_MODE )
+                runSingle( parse, urlString, new String( parse.getTitle() ) );
+        }
+        else { // the urlString is main page
+            Common.debugPrintln( "全集頁面（main page）" );
+            Common.isMainPage = true;
+
+            String allPageString = parse.getAllPageString( urlString );
+
+            parse.setTitle( parse.getTitleOnMainPage( urlString, allPageString ) );
+            Common.debugPrintln( "漫畫名稱: " + parse.getTitle() );
+            //Common.nowTitle = parse.getTitle();
+
+            Common.debugPrint( "開始解析解析各集位址和各集名稱：" );
+            List<List<String>> combinationList = parse.getVolumeTitleAndUrlOnMainPage( urlString, allPageString );
+            List<String> volumeList = combinationList.get( 0 );
+            List<String> urlList = combinationList.get( 1 );
+
+            Common.debugPrint( "  ......解析各集位址和各集名稱完畢!!" );
+            
+            parse.deleteTempFile( parse.getTempFileNames() );
+
+            parse.outputVolumeAndUrlList( volumeList, urlList );
+
+            //if ( Flag.allowDownloadFlag && !Flag.downloadingFlag ) {
+            if ( parse.getRunMode() == RunModeEnum.DOWNLOAD_MODE ) {
+                runSingle( parse, urlString, new String( parse.getTitle() ) );
+                System.out.println( "-------------------------------" );
+            }
+        }
+
+        if ( Flag.allowDownloadFlag )
+            System.out.println( "\nAll downloads are done!\n" );
+    }
+
+    private synchronized void runSingle( ParseOnlineComicSite parse, String urlString, String titleString ) {
+        if ( Run.isAlive ) {
+            Flag.downloadingFlag = true;
+
+            parse.setURL( urlString );
+            parse.setTitle( titleString );
+
+            parse.setParameters();
+            
+            if ( parse.siteID == Site.EH)
+                parse.setDownloadDirectory( SetUp.getOriginalDownloadDirectory() +
+                                         parse.getTitle() + Common.getSlash() );
+            else
+                parse.setDownloadDirectory( SetUp.getOriginalDownloadDirectory() +
+                                         parse.getTitle() + Common.getSlash() + 
+                                         parse.getWholeTitle() + Common.getSlash() );
+
+            // 若已存在同檔名壓縮檔，則不解析下載網址
+            if ( !existZipFile( parse.getDownloadDirectory() ) ) {
+                parse.parseComicURL(); // EH在此已經開始下載了。
+            }
+            String[] urls = parse.getComicURL();
+
+            //Common.nowTitle = parse.getTitle();
+
+            System.out.println( "Download Directory : " + parse.getDownloadDirectory() );
+            System.out.println( "\nReady to download ...\n" );
+
+
+            if ( SetUp.getOutputUrlFile() ) {
+                Common.debugPrintln( "允許輸出圖片位址" );
+                Common.outputUrlFile( urls, parse.getDownloadDirectory() );  // output url file
+            }
+
+            //if ( !SetUp.assignDownloadPath )
+            //    SetUp.setDownloadPath( parse.getDefaultDownloadPath() ); // set download directory
+
+            if ( parse.getSiteID() != Site.EH && SetUp.getDownloadPicFile() ) {
+                // 如果已經有同檔名壓縮檔存在，就假設已經下載完畢而不下載。
+                if ( !existZipFile( parse.getDownloadDirectory() ) ) {
+                    Common.debugPrintln( "開始下載整集：" );
+                    Common.downloadManyFile( urls, parse.getDownloadDirectory(),
+                                             SetUp.getPicFrontName(), "jpg" );
+                }
+            }
+
+            followingWork( parse );
+
+            Flag.downloadingFlag = false;
+        }
+    }
+
+    public boolean existZipFile( String downloadPicDirectory ) {
+        String file;
+        if ( downloadPicDirectory.lastIndexOf( Common.getSlash() ) == downloadPicDirectory.length() - 1 )
+            file = downloadPicDirectory.substring( 0, downloadPicDirectory.length() - 1 ) + ".zip";
+        else
+            file = downloadPicDirectory + ".zip";
+
+        //Common.debugPrintln( "將處理的壓縮檔名稱：" + file );
+
+        if ( new File( file ).exists() ) {
+            Common.debugPrintln( file + "已經存在!" );
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public synchronized void followingWork( ParseOnlineComicSite parse ) { // compress or not & delete or not
+        if ( Run.isAlive ) {
+            File downloadPath = new File( parse.getDownloadDirectory() );
+
+            if ( SetUp.getAutoCompress() ) { // compress to zip file or not
+                File zipFile = new File( downloadPath.getParent() + "/" +
+                                         parse.getWholeTitle() + ".zip" );
+
+                if ( !zipFile.exists() ) {
+                    Common.compress( downloadPath, zipFile );
+                    System.out.println( zipFile.getAbsolutePath() + " made!" );
+                }
+                else
+                    System.out.println( zipFile.getAbsolutePath() + " already made!" );
+            }
+
+            if ( SetUp.getDeleteOriginalPic() ) { // delete the whole folder or not
+                Common.deleteFolder( parse.getDownloadDirectory() );
+
+                System.out.println( parse.getDownloadDirectory() + " deletion!" );
+            }
+        }
+    }
+}
