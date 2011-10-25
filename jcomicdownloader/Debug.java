362704fda2d081502099644e476ede3632ac853c
abc9070410@gmail.com <abc9070410@gmail.com@eb843f33-aada-1633-33ce-5fc2b7a079e5>
2011-10-25 23:32:02 +0000

    1.07

---
+++ jcomicdownloader/Debug.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcomicdownloader;
+
+/**
+ *
+ * @author user
+ */
+public class Debug {
+    public static boolean debugMode = true;
+    public static boolean commandDebugMode = true;
+}
